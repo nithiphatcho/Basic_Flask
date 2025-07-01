e523ac19925824d3caaa0715f90153de2ee29ea9
Nithiphat <nithiphat.cho@gmail.com>
2025-07-01 21:18:37 +0700

    test

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Hello Flask Framework<h1> <h2>Third Commit<h2>"
+    return "<h1>Hello Flask Framework<h1> <h2>Test<h2>"
 
 if __name__ == "__main__":
     app.run()
