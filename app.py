a848df240f7ac5a2acbd4074967830a8cee3ccad
Nithiphat <nithiphat.cho@gmail.com>
2025-07-01 21:28:31 +0700

    BackToFirstExample

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "<h1>Hello Flask Framework<h1> <h2>Welcome to WebApp<h2> <h3>Nithiphat<h3>"
+    return "<h1>Hello Flask Framework<h1>"
 
 if __name__ == "__main__":
     app.run()
